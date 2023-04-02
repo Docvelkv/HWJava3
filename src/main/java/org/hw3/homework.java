3e1d565dc12a786ed4c63bdd7c166c81c4044be1
Docvel <docvel@yandex.ru>
2023-04-03 02:29:22 +0300

    in infrastructure added some methods

--- src/main/java/org/hw3/homework.java
+++ src/main/java/org/hw3/homework.java
@@ -1,7 +1,12 @@
 package org.hw3;
 
+import java.util.List;
+
 public class homework {
     public static void main(String[] args) {
-
+        List<Integer> lst1 = infrastructure.createListNumbers(10, -10, 9);
+        System.out.println(lst1);
+        List<String> lst2 = infrastructure.createMixedList(10,1, 5);
+        System.out.println(lst2);
     }
 }
--- src/main/java/org/hw3/infrastructure.java
+++ src/main/java/org/hw3/infrastructure.java
@@ -1,6 +1,7 @@
 package org.hw3;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
@@ -13,7 +14,7 @@ public class infrastructure {
      * @param maxNum максимальное значение
      * @return список целых чисел
      */
-    static List<Integer> createListNumbers(int len, int minNum, int maxNum) {
+    public static List<Integer> createListNumbers(int len , int minNum, int maxNum) {
         List<Integer> lstNum = new ArrayList<>();
         Random rnd = new Random();
         if (minNum == 0) {
@@ -29,7 +30,49 @@ public class infrastructure {
         return lstNum;
     }
 
+    /**
+     * Создание смешанного списка строк. Строки могут содержать символы или числа
+     * @param len длина списка
+     * @param minStr минимальное количество символов в одном элементе
+     * @param maxStr максимальное количество символов в одном элементе
+     * @return смешанный список
+     */
+    public static List<String> createMixedList(int len, int minStr, int maxStr) {
 
+        Random rnd = new Random();
+        int strRatio = rnd.nextInt(10);
+        int lenStr = len / 10 * strRatio;
+        int lenNum = len - lenStr;
+        List<String> tmpNum = new ArrayList<>();
+        for (int i = 0; i < lenNum; i++) {
+            int lenElement = rnd.nextInt(maxStr - minStr) + minStr;
+            int num = 0;
+            for (int j = 0; j <= lenElement; j++) {
+                num = num * 10 + rnd.nextInt(9);
+            }
+            num = num * (rnd.nextBoolean() ? -1 : 1);
+            tmpNum.add(String.format("%d", num));
+        }
+        List<Character> lstSymbols = new ArrayList<>();
+        Collections.addAll(lstSymbols, 'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z');
+        List<String> tmpStr = new ArrayList<>();
+        for (int i = 0; i < lenStr; i++) {
+            int lenElement = rnd.nextInt(maxStr - minStr) + minStr;
+            StringBuilder sb = new StringBuilder();
+            for (int j = 0; j <= lenElement; j++) {
+                int num = rnd.nextInt(26);
+                sb.append(lstSymbols.get(num));
+            }
+            tmpStr.add(sb.toString());
+        }
+        List<String> result = new ArrayList<>();
+        result.addAll(tmpStr);
+        result.addAll(lenStr, tmpNum);
+        Collections.shuffle(result);
+        return result;
+    }
 
     /**
      * Удалить все чётные элементы из списка
